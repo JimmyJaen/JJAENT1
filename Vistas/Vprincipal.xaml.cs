5a36a27c9bbee9918727e4e25883a35736979da5
jaenb <jaenb@JIMMY>
2024-05-05 21:45:10 -0500

    tarea 1

--- Vistas/Vprincipal.xaml.cs
+++ Vistas/Vprincipal.xaml.cs
@@ -6,4 +6,23 @@ public partial class Vprincipal : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void btnCalcularAporteClicked(object sender, EventArgs e)
+    {
+        // extraccion de valores ingresados
+        string nombre = txtNombre.Text;
+        string apellido = txtApellido.Text;
+        int edad = Convert.ToInt32(txtEdad.Text);
+        double salario = Convert.ToDouble(txtSalario.Text);
+
+        // Calculo del aporte al IESS
+        double aporteIESS = salario * 0.0945;
+
+        // Mostrar de recuadro con los detalles de ingreso
+        await DisplayAlert("Aportacion al IESS",
+                           $"Bienvenido {nombre} {apellido}!\n\n" +
+                           $"Tienes {edad} años.\n\n" +
+                           $"Tu aporte mensual al IESS es: ${aporteIESS.ToString("0.00")}",
+                           "Aceptar");
+    }
+
 }
